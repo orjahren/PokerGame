c964072ff045ed6d7a9dd378c60f0915f8a98ac4
jodashda <jodashda@gmail.com>
2018-01-19 11:42:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //entrypoint:
 "use strict";
-const http = require("http";
+const http = require("http");
 const fs = require("fs");
 
 let server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"};
+  res.writeHead(200, {"Content-Type": "text/plain"});
  // res.write(fs.readFileSync("index.html"));
  res.write("Hello world!");
   res.end();
