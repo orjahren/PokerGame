2d74e7f887eb4b139e39a3ac3b11848cc0cf95dd
jodashda <jodashda@gmail.com>
2018-01-19 09:58:53 +0100

    Create server.js
    server

---
+++ server.js
@@ -0,0 +1,12 @@
+//entrypoint:
+"use strict";
+const http = require("http";
+const fs = require("fs");
+
+let server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/plain"};
+ // res.write(fs.readFileSync("index.html"));
+ res.write("Hello world!");
+  res.end();
+}).listen(80);
+console.log("Server kjører!");
